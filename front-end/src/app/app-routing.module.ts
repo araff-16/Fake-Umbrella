84d5331fe172c6303a5e351ff05ca2a35c75e561
Alex Raffa <alex_raffa16@hotmail.com>
2022-02-03 13:46:28 -0500

    add components to routing

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CustomersComponent } from './Components/customers/customers.component';
+import { RainingInfoComponent } from './Components/raining-info/raining-info.component';
+import { TopFourComponent } from './Components/top-four/top-four.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "customers", component: CustomersComponent},
+  {path: "raininginfo", component: RainingInfoComponent},
+  {path: "topfour", component: TopFourComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
