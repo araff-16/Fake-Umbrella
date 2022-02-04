37aac8ad3a5f91a54f31583548a1c3c7127dc672
Alex Raffa <alex_raffa16@hotmail.com>
2022-02-03 19:41:26 -0500

    update and add customers

--- back-end/src/customers/customers.controller.ts
+++ back-end/src/customers/customers.controller.ts
@@ -1,9 +1,61 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 
 @Controller('customers')
 export class CustomersController {
   constructor(private customersService: CustomersService) {}
+
+  @Post()
+  addCustomer(
+    @Body('company') company: string,
+    @Body('contact') contact: string,
+    @Body('telephone') telephone: string,
+    @Body('city') city: string,
+    @Body('province') province: string,
+    @Body('country') country: string,
+    @Body('address') address: string,
+    @Body('postal') postal: string,
+    @Body('employees') employees: number,
+  ) {
+    return this.customersService.insertCustomer(
+      company,
+      contact,
+      telephone,
+      city,
+      province,
+      country,
+      address,
+      postal,
+      employees,
+    );
+  }
+
+  @Put(':id')
+  updateCustomer(
+    @Param('id') id: string,
+    @Body('company') company: string,
+    @Body('contact') contact: string,
+    @Body('telephone') telephone: string,
+    @Body('city') city: string,
+    @Body('province') province: string,
+    @Body('country') country: string,
+    @Body('address') address: string,
+    @Body('postal') postal: string,
+    @Body('employees') employees: number,
+  ) {
+    return this.customersService.updateCustomer(
+      parseInt(id),
+      company,
+      contact,
+      telephone,
+      city,
+      province,
+      country,
+      address,
+      postal,
+      employees,
+    );
+  }
 
   @Get()
   getCustomers() {
--- back-end/src/customers/customers.service.ts
+++ back-end/src/customers/customers.service.ts
@@ -11,4 +11,55 @@ export class CustomersService {
   getCustomers() {
     return [...this.customers];
   }
+  insertCustomer(
+    company: string,
+    contact: string,
+    telephone: string,
+    city: string,
+    province: string,
+    country: string,
+    address: string,
+    postal: string,
+    employees: number,
+  ) {
+    const newCustomer = new Customer(
+      company,
+      contact,
+      telephone,
+      city,
+      province,
+      country,
+      address,
+      postal,
+      employees,
+    );
+    this.customers.push(newCustomer);
+    return newCustomer;
+  }
+  updateCustomer(
+    id: number,
+    company: string,
+    contact: string,
+    telephone: string,
+    city: string,
+    province: string,
+    country: string,
+    address: string,
+    postal: string,
+    employees: number,
+  ) {
+    const updateCustomer = new Customer(
+      company,
+      contact,
+      telephone,
+      city,
+      province,
+      country,
+      address,
+      postal,
+      employees,
+    );
+    this.customers[id] = updateCustomer;
+    return updateCustomer;
+  }
 }
