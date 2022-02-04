719bdf6860e25bb9e3fcd9e9bbb7859f984d1c13
Alex Raffa <alex_raffa16@hotmail.com>
2022-02-03 21:01:41 -0500

    remder companies with rainy days

--- front-end/src/app/Components/raining-info/raining-info.component.ts
+++ front-end/src/app/Components/raining-info/raining-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/Services/http.service';
 
 @Component({
   selector: 'app-raining-info',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RainingInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
+
+  rainingCustomers: any[]
+
+  loading = true
 
   ngOnInit() {
+    this.httpService.getRequest('http://localhost:3000/customers/raining')
+    .subscribe((data) => {
+      this.rainingCustomers = data
+      this.loading = false
+    })
   }
-
 }
